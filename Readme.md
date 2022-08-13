4b26fb2af2fc138673cd7519cecaf67d2cc28b7f
amontalvof <amontalvof92@gmail.com>
2022-08-13 13:52:00 -0500

    feat: added section 3

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Java Development Bootcamp
+
+## Technologies
+
+-   Java
---
+++ Section-3-Initials/Initials.java
@@ -0,0 +1,12 @@
+public class Initials {
+    // Before looking at the solution, try the challenge on your own.
+    public static void main(String[] args) {
+        System.out.println("  A    M   M");
+        System.out.println(" A A   MM MM");
+        System.out.println("A   A  M M M");
+        System.out.println("AAAAA  M   M");
+        System.out.println("A   A  M   M");
+        System.out.println("A   A  M   M");
+        System.out.println("A   A  M   M");
+    }
+}
